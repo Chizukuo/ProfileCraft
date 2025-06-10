0ee3bc702290708c911b1f4e362887a8839e18dc
chizukuo <chizukuo@icloud.com>
2025-06-10 22:08:47 +0800

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ProfileCraft/', 
   plugins: [react()],
   server: {
+    base: '/ProfileCraft/', 
     host: '0.0.0.0',
     port: 5173
   }
